f9923e3b2a5d9ee6b927cbc400002e6b9368fee7
Dan Brook <broq@cpan.org>
2013-03-26 21:35:30 +0100

    Test for meaningful type error messages.

--- spec/reindeer/types_spec.rb
+++ spec/reindeer/types_spec.rb
@@ -6,7 +6,7 @@ describe 'Reindeer types' do
       with Reindeer::Role::TypeConstraint
 
       def verify(val)
-        val =~ /^[a-z]+$/
+        val.downcase == val
       end
 
       meta.compose!
@@ -22,5 +22,32 @@ describe 'Reindeer types' do
     expect {
       obj = SoftlySpoken.new(start: 'LOUD NOISES')
     }.to raise_error(Reindeer::TypeConstraint::Invalid)
+  end
+
+  it 'should have useful a error message' do
+    class LoudWord < Reindeer
+      with Reindeer::Role::TypeConstraint
+
+      def verify(val)
+        val.upcase == val
+      end
+
+      def error_message_for(val)
+        "THE VALUE '#{val}' WASN'T LOUD ENOUGH"
+      end
+
+      meta.compose!
+    end
+
+    class SergeantMajor < Reindeer
+      has :order, is_a: String, type_of: LoudWord
+    end
+
+    obj = SergeantMajor.new(order: 'TEN HUT')
+    expect(obj.order).to eq('TEN HUT')
+
+    expect {
+      SergeantMajor.new(order: 'quiet please')
+    }.to raise_error(Reindeer::TypeConstraint::Invalid, /WASN'T LOUD ENOUGH/)
   end
 end
